974af1fcb34445a7b94d7a1cf0d227dd4bb35939
Perzach <per-oystein.zachariassen@dnb.no>
2023-03-21 08:21:55 +0100

    feat: add raycast script to start screen saver

---
+++ raycast_common/start-screen-saver.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Start Screen Saver
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🔒
+
+/System/Library/CoreServices/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine
