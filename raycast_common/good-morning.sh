14888650e39f15f82cdef16cc0b81b68ee97fd26
Perzach <per-oystein.zachariassen@dnb.no>
2024-02-22 10:13:00 +0100

    feat: added good-morning raycast script

---
+++ raycast_common/good-morning.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Good morning
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon ☕️
+
+open -a "Google Chrome" --args --profile-directory="Profile 1"
+open -a "Slack"
+open -a "Microsoft Teams (work or school)"
+open -a "Microsoft Outlook"
+open -a "Visual Studio Code"
+open -a "iTerm"
+open -a "Notes"
+
+# Open news pages
+sleep 5
+open -na "Google Chrome" --args --new-window \
+"https://www.tek.no/" \
+"https://hckrnews.com/" \
+"https://www.nrk.no/" \
+"https://www.dn.no/" \
+"https://bt.no/" \
+"https://dnb.workplace.com/" 
